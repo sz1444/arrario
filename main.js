32394a6d0a0f568173f16b37bad5b8655457d84d
Arkadiusz Wierzbicki <sz144@wp.pl>
2018-11-19 21:01:44 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).on('ready', function () {
         responsive: [{
                 breakpoint: 600,
                 settings: {
-                    arrows: false,
+                     arrows: false,
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
@@ -59,121 +59,80 @@ $(document).on('ready', function () {
   ]
     });
 });
-$(function() {
-    var $inputs = $('form input[required], form textarea[required], select[required]');
-
-    var displayFieldError = function($elem) {
-        var $fieldRow = $elem.closest('.form-row');
-        var $fieldError = $fieldRow.find('.field-error');
-        if (!$fieldError.length) {
-            var errorText = $elem.attr('data-error');
-            var $divError = $('<div class="field-error">' + errorText + '</div>');
-            $fieldRow.append($divError);
-        }
-    };
-
-    var hideFieldError = function($elem) {
-        var $fieldRow = $elem.closest('.form-row');
-        var $fieldError = $fieldRow.find('.field-error');
-        if ($fieldError.length) {
-            $fieldError.remove();
-        }
-    };
-
-    $inputs.on('input keyup', function() {
-        var $elem = $(this);
-        if (!$elem.get(0).checkValidity()) {
-            $elem.addClass('error');
-        } else {
-            $elem.removeClass('error');
-            hideFieldError($elem);
-        }
-    });
-
-    $inputs.filter(':checkbox').on('click', function() {
-        var $elem = $(this);
-        var $row = $(this).closest('.form-row');
-        if ($elem.is(':checked')) {
-            $elem.removeClass('error');
-            hideFieldError($elem);
-        } else {
-            $elem.addClass('error');
-        }
-    });
+$(function () {
+   
     $('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { 
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); 
-            $target.focus(); 
-          };
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
         });
-      }
-    }
-  });
-    
-    $(".menu-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden-menu');
-});
-    
+
+    $(".menu-toggle").on('click', function () {
+        $(this).toggleClass("on");
+        $('.menu-section').toggleClass("on");
+        $("nav ul").toggleClass('hidden-menu');
+    });
+
 })
 
 
 window.addEventListener('scroll', navColor);
 document.querySelector('.call').addEventListener('click', openAlert);
 document.querySelector('.close').addEventListener('click', closeAlert);
+
 function navColor() {
 
     if (pageYOffset > 100) {
         document.querySelector('nav').classList.add('nav');
-        document.querySelector('.logo a img').src="img/logo.png";
+        document.querySelector('.logo a img').src = "img/logo.png";
     } else {
         document.querySelector('nav').classList.remove('nav');
-         document.querySelector('.logo a img').src="img/logowhite.png";
+        document.querySelector('.logo a img').src = "img/logowhite.png";
     }
 }
-var phone =document.querySelector('.number');
+var phone = document.querySelector('.number');
 
 phone.addEventListener('keypress', noNumbers);
 
-function noNumbers(e)
-{
- if ((e.which < 48 || e.which > 57)) {
-            e.preventDefault();
-    }}
+function noNumbers(e) {
+    if ((e.which < 48 || e.which > 57)) {
+        e.preventDefault();
+    }
+}
 
 document.addEventListener('DOMContentLoaded', loadEnd);
 
-function loadEnd(){
+function loadEnd() {
     document.querySelector('.load').classList.add('hidden');
 }
 
-function openAlert(){
+function openAlert() {
     document.querySelector('.alert').classList.remove('hidden');
 }
 
 
-function closeAlert(){
+function closeAlert() {
     document.querySelector('.alert').classList.add('hidden');
 }
-
