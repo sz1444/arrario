d45e26e355af82d3e37119880ccafdd05ae6e7d7
Arkadiusz Wierzbicki <sz144@wp.pl>
2018-11-22 16:01:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -130,10 +130,3 @@ function openAlert() {
 function closeAlert() {
     document.querySelector('.alert').classList.add('hidden');
 }
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
